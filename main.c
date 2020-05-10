9c395b096175d46bd340150e0b47dc820bc9f293
gulmetin <58664172+gulmetin@users.noreply.github.com>
2020-05-10 15:33:58 +0300

    Otopark sistemi
    Araç bilgisi ve giriş saatini alan bir sistemdir. Fonksiyonlar ve struct kullanılmıştır.

--- main.c
+++ main.c
@@ -1,46 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    srand(time(NULL));
-    int sayilar[10], *diziPtr;
-    int diziTek[10], *diziPT;
-    int diziCift[10], *diziPC;
-    int a=0,b=0; //tek ve cift atamasý icin
-
-    diziPtr=sayilar;
-    diziPT=diziTek;
-    diziPC=diziCift;
-    printf("Sayilar:\n");
-    for(int i=0;i<10;i++)
-    {
-        *(diziPtr+i)= 1+rand()%100;
-        printf("%d\n",*(diziPtr+i));
-
-        if(*(diziPtr+i)%2==0){
-         *(diziPC+a)= *(diziPtr+i);
-         a++;
-        }
-        else{
-         *(diziPT+b)= *(diziPtr+i);
-         b++;
-        }
-    }
-    printf("\nCift sayilar:\n");
-    EkranaYazdir(diziCift,a);
-    printf("\nTek sayilar:\n");
-    EkranaYazdir(diziTek,b);
-
+typedef struct{
+   char plaka[15];
+   char renk[50];
+   char model[50];
+} ARACBILGI;
+typedef struct{
+    char saat[10];
+} GIRIS;
+ARACBILGI arac_oz[1000];
+GIRIS arac_giris[1000];
+int s=0,a=0; //s=Arac sayisini gösterir.
+int main(){
+   Anamenu();
     return 0;
-
 }
-void EkranaYazdir(int dizi[],int n)
-{
-    int *pdizi;
-    pdizi=dizi;
-    for(int i=0;i<n;i++)
-    {
-        printf("%d\n", *(pdizi+i));
+void AracEkle(){
+   int i;
+   printf("\nKac Arac Eklemek Istersiniz?\n");
+   scanf("%d",&i);
+   s=s+i;
+
+   for(a;a<s;a++){
+    printf("\nPlakayi giriniz.\n");
+    scanf("%s",&arac_oz[a].plaka);
+    printf("Modeli giriniz.\n");
+    scanf("%s",&arac_oz[a].model);
+    printf("Renk giriniz.\n");
+    scanf("%s",&arac_oz[a].renk);
+    printf("Giris saati giriniz.\n");
+    scanf("%s",&arac_giris[a].saat);
+   }
+   Anamenu();
+}
+void AraclariListele(){
+    for(int i=0; i<s; i++){
+        printf("\nARAC %d BILGILERI\n", i+1);
+        printf("Plaka:"); puts(arac_oz[i].plaka);
+        printf("Model:"); puts(arac_oz[i].model);
+        printf("Renk:");  puts(arac_oz[i].renk);
+        printf("Giris Saati:"); puts(arac_giris[i].saat);
+    }
+  Anamenu();
+}
+void Cikis(){
+    exit(0);
+}
+void Anamenu(){
+    int b;
+    printf("\nMevcut arac sayisi: %d \n",s);
+    printf(" Arac eklemek icin 1\n Araclari listelemek icin 2\n Cikis icin 3'e basiniz.\n");
+    scanf("%d",&b);
+    switch(b){
+    case 1:
+        AracEkle();
+    case 2:
+        AraclariListele();
+    case 3:
+         Cikis();
     }
 }
